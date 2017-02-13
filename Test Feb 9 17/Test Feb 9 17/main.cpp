c0f47374edb608e5bcdd8be749df44b72c9376b5
sean_ryall <w0293490@nscc.ca>
2017-02-13 17:30:12 -0400

    More progress...

--- Test Feb 9 17/Test Feb 9 17/main.cpp
+++ Test Feb 9 17/Test Feb 9 17/main.cpp
@@ -7,38 +7,29 @@
 //
 
 #include <iostream>
-
 #include <fstream>
-
-#include <vector>
-
-#include <vector>
+#include <sstream>
 #include <string>
-#include <sstream>
-#include <fstream>
-#include <iostream>
 #include <stdio.h>
-
-#include <sstream>
 #include <stdlib.h>
-#include "stacknode.hpp"
-#include "stdafx.hpp"
-#include <iostream>
 #include <stack>
-
-//#include "stdafx.h"
+#include <time.h>
 
 using namespace std;
 
-//ostream & operator<<(ostream & os, stack<pair<int, int> > mazeStack) //function header
-//{
-//    while(!mazeStack.empty()) //body
-//    {
-//        os << &mazeStack.top() << " ";
-//        mazeStack.pop();
-//    }
-//    return os; // end of function
-//}
+
+//https://ubuntuforums.org/showthread.php?t=1357606
+int msleep(unsigned long milisec)
+{
+    struct timespec req={0};
+    time_t sec=(int)(milisec/1000);
+    milisec=milisec-(sec*1000);
+    req.tv_sec=sec;
+    req.tv_nsec=milisec*1000000L;
+    while(nanosleep(&req,&req)==-1)
+        continue;
+    return 1;
+}
 
 
 int main(int argc, char** argv) {
@@ -49,49 +40,64 @@ int main(int argc, char** argv) {
     // for demo set to 5x4
     int row = 0;
     int col = 0;
-//    char data[100];
     
+    
+    //Open original file according to argv, delete /r's, the save it to new temp file.
     // open a file in read mode.
     ifstream infile;
-    infile.open("maze.txt");
-//    infile >> data;
+    if (!strcmp(argv[1], "maze.txt"))
+    {
+        infile.open("maze.txt");
+    }
+    else if (!strcmp(argv[1], "maze2.txt"))
+    {
+        infile.open("maze2.txt");
+    }
+    else if (!strcmp(argv[1], "maze3.txt"))
+    {
+        infile.open("maze3.txt");
+    }
+    else if (!strcmp(argv[1], "maze4.txt"))
+    {
+        infile.open("maze4.txt");
+    }
+    else if (!strcmp(argv[1], "mazex.txt"))
+    {
+        infile.open("mazex.txt");
+    }
+    
     
     // open a file in write mode.
     ofstream outfile;
     outfile.open("temp.txt");
-    // write data into the file.
-//    outfile << data << endl;
     
+    // write data into the file.
     string data;
     while (getline(infile, data))
     {
         //erase all '\r's from file
         data.erase(remove(data.begin(), data.end(), '\r'), data.end());
-//        data.erase(data.length()-2);
         outfile << data << endl;
     }
-    
     
     infile.close();
     outfile.close();
     
     
-    
     string mazeText = "temp.txt";
     
+    //find number of rows and cols.
     //http://stackoverflow.com/questions/9132151/how-to-find-number-of-characters-in-a-file-without-traversing-the-contents
     FILE*  file = fopen("temp.txt", "rb");
     fseek(file, 0, SEEK_END);
     size_t  size = ftell(file);      // This is the size of the file.
-    cout << "size is " << size << endl;
+//    cout << "size is " << size << endl;
     // But note it is in bytes.
     // Also note if you are reading it into memory this is
     // is the value you want unless you plan to dynamically
     // convert the character encoding as you read.
-    
     fseek(file, 0, SEEK_SET);        // Move the position back to the start.
     
-
    //http://stackoverflow.com/questions/11387370/how-can-i-safely-convert-unsigned-long-int-to-int
     stringstream ss;
     ss << size;
@@ -111,57 +117,21 @@ int main(int argc, char** argv) {
     myfile.close();
     
     ifstream myfile3(mazeText);
-//    while (getline(myfile3, line))
-//    {
-//        for (char& c : line)
-//        {
-//            if (c == '\r')
-//            {
-//                col = ((totalNumberOfChars - ((numberOfLines * 2) - 1))  / (numberOfLines)) + 3;
-//            }
-//            else
-//            {
-//                col = ((totalNumberOfChars - (numberOfLines - 1))  / (numberOfLines)) + 1;
-//            }
-//        }
-//    }
-    
-//    while (getline(file, line))
-//    {
-//        //trim the line:
-//        line.erase(line.find_last_not_of(" \n\r\t") + 1);
-//        
-//        for (char& c : line)
-//        {
-//            cout << c;
-//        }
-//        cout << "[ENDLINE]\n";
-//    }
-    
-    cout << "Number of lines in text file: " << numberOfLines << endl;
+
+//    cout << "Number of lines in text file: " << numberOfLines << endl;
     
     col = ((totalNumberOfChars - (numberOfLines - 1))  / (numberOfLines)) + 1;
-//    col = ((totalNumberOfChars - ((numberOfLines * 2) - 1))  / (numberOfLines)) + 3;
-    
     row = numberOfLines;
     
-    cout << "col = " << col << endl;
-    cout << "row = " << row << endl;
-    
-    
-    
+//    cout << "col = " << col << endl;
+//    cout << "row = " << row << endl;
     
     myfile.close();
     
     
-
-//    col = 7;
-//    row = 4;
-    
     // dynamically allocate an array
     // row = number of maze rows
     // col = line length
-    
     maze = new char *[row];
 
     for (int count = 0; count < row; count++)
@@ -169,10 +139,7 @@ int main(int argc, char** argv) {
             maze[count] = new char[col];
         }
         
-        // load the elements in to the maze
-//        cout << endl << "Now enter the element for the maze...";
-    
-    
+    // load the elements in to the maze from the temp text file.
     fstream fin(mazeText);
     for (int i = 0; i < row; i++)
     {
@@ -180,185 +147,51 @@ int main(int argc, char** argv) {
         {
             //No skipping white-space (noskipws)...
             fin >> noskipws >> maze[i][j];
-            cout << maze[i][j];
+//            cout << maze[i][j];
         }
-//           cout << endl;
             
     }
     fin.close();
     
+//    cout << "The chosen coord is: " << maze[2][2] << endl;
     
-    cout << endl << endl;
-    cout << "The chosen coord is: " << maze[2][2] << endl;
-        
     // Write them back out
-    
-    cout << "Here is your matrix or maze" << endl;
+    cout << "Here is your maze" << endl;
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
             cout << maze[i][j];
-            
-            
         }
     }
-    cout << endl << endl << endl;
+    cout << endl << endl;
     
+    //Set endpoint.
     maze[row-2][col-2] = 'F';
-//    maze[2][2] = 'T';
-//    maze[0][2] = 'B';
-    
-//    maze[1][0] = '#';
-//    maze[1][1] = '#';
-//    maze[1][2] = '#';
-//    maze[1][3] = '#';
-//    maze[1][4] = '#';
-    
-    cout << "Now here is your matrix or maze" << endl;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cout << maze[i][j];
-            
-            
-        }
-    }
-    cout << endl << endl << endl;
-    
-    
-    
-    
-    
-//    for (int i = 0; i < row; i++)
-//    {
-//        for (int j = 0; j < col; j++)
-//        {
-//            cout << maze[i][j];
-////            
-//        }
-//    }
-    cout << endl << endl << endl;
-    
-    
-    
-    
-//    int co1 = 1;
-//    int co2 = 2;
-    
+  
+    //Create stack.
     stack<pair<int, int> > mazeStack;
-//    mazeStack.push(make_pair(co1, co2));
-//    mazeStack.push(make_pair(1,0));
-//    mazeStack.push(make_pair(1,1));
-//    mazeStack.push(make_pair(1,2));
-//    mazeStack.push(make_pair(1,3));
-//    mazeStack.push(make_pair(1,4));
-//    mazeStack.push(make_pair(1,5));
     
-    long stackSize = mazeStack.size();
+//    long stackSize = mazeStack.size();
     
-    cout << "Size of stack is " << stackSize << endl;
+//    cout << "Size of stack is " << stackSize << endl;
     
-//    for (int i = 0; i < row; i++)
-//    {
-//        for (int j = 0; j < col; j++)
-//        {
-//            if (maze[i][j] == ' ')
-//            {
-//                mazeStack.push(make_pair(i, j));
-//                maze[i][j] = '#';
-//            }
-//            else if (maze[i][j] == '|')
-//            {
-//                if (maze[i + 1][j] == ' ')
-//                {
-//                    maze[i + 1][j] = '#';
-//                }
-//                else if (maze[i - 1][j] == ' ')
-//                {
-//                    maze[i - 1][j] = '#';
-//                }
-//            }
-//            
-//        }
-//    }
-    
-    
-    cout << "Now here is your matrix or maze" << endl;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cout << maze[i][j];
-            
-            
-        }
-    }
-    cout << endl << endl << endl;
-    
-    
-    
-    
-    //    pair <int, int> p = mazeStack.top();
-//    for (int i = 0; i < stackSize; i++)
-//    {
-//        cout << p.first << "," << p.second << endl;
-//        mazeStack.pop();
-//    }
-    
-    
-    //test fill up stack with coords...
-    
-//    for (int i = 0; i < row; i++) {
-//        for (int j = 0; j < col; j++) {
-//            if (maze[i][j] == ' ') {
-//                mazeStack.push(make_pair(i,j));
-//                if (maze[i][j] == 'F') {
-//                    break;
-//                }
-//            }
-//        }
-//    }
-    
-    
-    
-    
-//    int i = 0;
-//    int j = 0;
-//    while (maze[i][j] != 'T') {
-//        for (i = 0; i < row; i++) {
-//            for (j = 0; j < col; j++) {
-//                if (maze[i][j] == 'T') {
-//                    break;
-//                }
-//                if (maze[i][j] == ' ') {
-////                      mazeStack.push(make_pair(1,5));
-//                    mazeStack.push(make_pair(i,j));
-//                }
-//            }
-//        }
-//        break;
-//    }
-    
-//    maze[row-2][col-2] = 'F';
-//    maze[1][0] = 'S';
-//    maze [3][5] = 'T';
+    //Figure out solution...
     mazeStack.push(make_pair(1, 0));
-    for (int i = 0; i < 10000; i++){
+    for (int i = 0; i < totalNumberOfChars; i++){
         pair<int, int> z = mazeStack.top();
         
         int x = z.first;
         int y = z.second;
     
-        
         if (maze[x][y+1] == 'F') {
             mazeStack.push(make_pair(x, y+1));
             maze[x][y+1] = 'D';
             cout << "YOU MADE IT!!!!!" << endl;
+            //replace '.'s with ' 's.
             for (int a = 0; a < row; a++) {
                 for (int b = 0; b < col; b++) {
-                    if (maze[a][b] == '.') {
+                    if (maze[a][b] == '*') {
                         maze[a][b] = ' ';
                     }
                 }
@@ -367,53 +200,96 @@ int main(int argc, char** argv) {
         }
         else if (maze[x][y+1] == ' ') {
             mazeStack.push(make_pair(x, y+1));
-            maze[x][y+1] = '.';
+            maze[x][y+1] = 'o';
         }
         else if (maze[x+1][y] == ' ') {
             mazeStack.push(make_pair(x+1, y));
-            maze[x+1][y] = '.';
+            maze[x+1][y] = 'o';
         }
         else if (maze[x][y-1] == ' ') {
             mazeStack.push(make_pair(x, y-1));
-            maze[x][y-1] = '.';
+            maze[x][y-1] = 'o';
         }
         else if (maze[x-1][y] == ' ') {
             mazeStack.push(make_pair(x-1, y));
-            maze[x-1][y] = '.';
+            maze[x-1][y] = 'o';
         }
         else{
             mazeStack.pop();
         }
+        
+//        cout << string(50, '\n');
+        cout << "Here is your maze" << endl;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                cout << maze[i][j];
+            }
+        }
+        msleep(2);
+        
     }
     
-    
-    cout << "Here is the contents of the stack..." << endl;
+    //Get the contents of the stack...add stack solution coords to maze...
+//    cout << "Here is the contents of the stack..." << endl;
     while(!mazeStack.empty())
     {
         pair<int, int> w = mazeStack.top();
-        cout << w.first << "," << w.second << endl;
+//        cout << w.first << "," << w.second << endl;
         maze[w.first][w.second] = '#';
-        
         mazeStack.pop();
     }
     
-    cout << "Lastly, here is your matrix or maze" << endl;
+    cout << "Here is the solved maze:" << endl;
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
             cout << maze[i][j];
-            
-            
         }
     }
-    cout << endl << endl << endl;
+    cout << endl << endl;
+    
+    // open a file in write mode.
+    ofstream finalFile;
+    
+    string oFile;
+    if (!strcmp(argv[1], "maze.txt"))
+    {
+        oFile = "mazeSolved.txt";
+    }
+    else if (!strcmp(argv[1], "maze2.txt"))
+    {
+        oFile = "maze2Solved.txt";
+    }
+    else if (!strcmp(argv[1], "maze3.txt"))
+    {
+        oFile = "maze3Solved.txt";
+    }
+    else if (!strcmp(argv[1], "maze4.txt"))
+    {
+        oFile = "maze4Solved.txt";
+    }
+    else if (!strcmp(argv[1], "mazex.txt"))
+    {
+        oFile = "mazexSolved.txt";
+    }
+    
+    finalFile.open(oFile);
+    
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            finalFile << maze[i][j];
+        }
+    }
     
     
     
     
-    
-    cout << "Deleting maze and terminating" << endl;
+//    cout << "Deleting maze and terminating" << endl;
     
     
     // To free the dynamically allocated memory
@@ -437,9 +313,8 @@ int main(int argc, char** argv) {
         maze = NULL;
     
     
-    // Write them back out after deletion...
-    cout << "Here is your matrix or maze after deletionnnnnnnnnnnnn" << endl;
-    cout << "\033[1;31mbold red text\033[0m\n";
+//    // Write them back out after deletion...
+//    cout << "Here is your matrix or maze after deletionnnnnnnnnnnnn" << endl;
     
     
         return 0;
